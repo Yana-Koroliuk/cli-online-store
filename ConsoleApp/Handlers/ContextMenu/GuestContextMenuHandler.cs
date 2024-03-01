8b0aaa6339d2d96be96e087d6c359a75d2f6342f
Yurii Antonov <yurii_antonov@epam.com>
2024-03-01 18:27:30 +0200

    Initial commit

---
+++ ConsoleApp/Handlers/ContextMenu/GuestContextMenuHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp.Handlers.ContextMenu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StoreBLL.Interfaces;
+using StoreBLL.Models;
+
+public class GuestContextMenuHandler : ContextMenuHandler
+{
+    public GuestContextMenuHandler(ICrud service, Func<AbstractModel> readModel)
+        : base(service, readModel)
+    {
+    }
+
+    public override (ConsoleKey id, string caption, Action action)[] GenerateMenuItems()
+    {
+        (ConsoleKey id, string caption, Action action)[] array =
+            {
+                (ConsoleKey.V, "View Details", this.GetItemDetails),
+            };
+        return array;
+    }
+}
---
+++ StoreDAL/Data/StoreDbFactory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using StoreDAL.Data.InitDataFactory;
+
+namespace StoreDAL.Data
+{
+    public class StoreDbFactory
+    {
+        private readonly AbstractDataFactory factory;
+
+        public StoreDbFactory(AbstractDataFactory factory)
+        {
+              this.factory = factory;
+        }
+
+        public StoreDbContext CreateContext()
+        {
+            var context = new StoreDbContext(this.CreateOptions(), this.factory);
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+            return context;
+        }
+
+        public DbContextOptions<StoreDbContext> CreateOptions()
+        {
+            return new DbContextOptionsBuilder<StoreDbContext>()
+                .UseSqlite(CreateConnectionString())
+                .Options;
+        }
+
+        private static string CreateConnectionString()
+        {
+            var dbPath = "store.db";
+            var conString = new SqliteConnectionStringBuilder { DataSource = dbPath, Mode = SqliteOpenMode.ReadWriteCreate }.ConnectionString;
+            return conString;
+        }
+    }
+}
